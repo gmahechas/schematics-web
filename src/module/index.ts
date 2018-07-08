f93b61955d4c6625abc36dbce09a9f7cbc05bce1
gmahechas <gmahechas@ingenieros.com>
2018-07-08 16:33:06 -0500

    Fix id in shared

--- src/module/index.ts
+++ src/module/index.ts
@@ -8,13 +8,13 @@ export default function (options: any): Rule {
     (_tree: Tree, context: SchematicContext) => {
       context.logger.info('Module Schematic: ' + JSON.stringify(options));
     },
-/*     schematic('components', { name: options.name }),
+    schematic('components', { name: options.name }),
     schematic('containers', { name: options.name }),
     schematic('graphql', { name: options.name }),
     schematic('guards', { name: options.name }),
     schematic('models', { name: options.name }),
     schematic('services', { name: options.name }),
-    schematic('shared', { name: options.name }), */
+    schematic('shared', { name: options.name }),
     schematic('store', { name: options.name }),
     mergeWith(apply(url('./files'), [
       template({ ...stringUtils, ...options }),
