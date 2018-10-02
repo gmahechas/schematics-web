6fac4eba036a59563484a16e09ba878819ab350f
gmahechas <gmahechas@ingenieros.com>
2018-10-02 18:04:13 -0500

    add store testing to store schematic

---
+++ src/store/files/actions/entity-__name__.actions.spec.ts
@@ -0,0 +1,7 @@
+// import { Entity<%= classify(name) %> } from './entity-<%= name %>.actions';
+
+describe('Entity<%= classify(name) %>', () => {
+  it('should create an instance', () => {
+    // expect(new Entity<%= classify(name) %>()).toBeTruthy();
+  });
+});
