1e7f5bc35a22679e725faa888e4ea04f2cdd8a9d
gmahechas <gmahechas@ingenieros.com>
2019-01-29 15:46:49 -0500

    fix exports components in index
    via https://github.com/gmahechas/web/commit/55d0e4e1dfb8d9eb635d1749c9f5dc5ef6088eaa

--- src/components/files/index.ts
+++ src/components/files/index.ts
@@ -5,6 +5,3 @@ export const components: any[] = [
     SearchForm<%= classify(name) %>Component,
     Form<%= classify(name) %>Component
 ];
-
-export * from '@web/app/<%= path %>/<%= dasherize(name) %>/components/search-form-<%= dasherize(name) %>/search-form-<%= dasherize(name) %>.component';
-export * from '@web/app/<%= path %>/<%= dasherize(name) %>/components/form-<%= dasherize(name) %>/form-<%= dasherize(name) %>.component';
--- src/shared/files/index.ts
+++ src/shared/files/index.ts
@@ -3,5 +3,3 @@ import { DropdownPage<%= classify(name) %>Component } from '@web/app/<%= path %>
 export const shared: any[] = [
   DropdownPage<%= classify(name) %>Component
 ];
-
-export * from '@web/app/<%= path %>/<%= dasherize(name) %>/shared/dropdown-page-<%= dasherize(name) %>/dropdown-page-<%= dasherize(name) %>.component';
