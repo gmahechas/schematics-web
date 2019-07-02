f7dd6177fa6cebe8d87d6ba540fa69187d5641b1
gmahechas <gmahechas@ingenieros.com>
2019-07-02 14:10:16 -0500

    fix selected

--- src/store/files/reducers/layout-__name@dasherize__.reducer.ts
+++ src/store/files/reducers/layout-__name@dasherize__.reducer.ts
@@ -20,7 +20,7 @@ export const reducer = createReducer(
     from<%= classify(name) %>Actions.EntityActions.SetSelected,
     (state, { selected }) => ({
       ...state,
-      selected
+      selected: { ...state.selected, ...selected }
     })
   ),
   on(
